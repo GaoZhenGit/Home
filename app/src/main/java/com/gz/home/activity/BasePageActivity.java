e2dfa7abb36be097cfd8b2d86c218234e4cb77d0
GaoZhen <912815282@qq.com>
2015-08-18 22:14:43 +0800

    first commit

---
+++ app/src/main/java/com/gz/home/activity/BasePageActivity.java
@@ -0,0 +1,7 @@
+package com.gz.home.activity;
+
+/**
+ * Created by host on 2015/8/18.
+ */
+public class BasePageActivity {
+}
---
+++ app/src/main/java/com/gz/home/app/HomeApplication.java
@@ -0,0 +1,7 @@
+package com.gz.home.app;
+
+/**
+ * Created by host on 2015/8/18.
+ */
+public class HomeApplication {
+}
